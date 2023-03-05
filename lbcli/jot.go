462b00cd4c35d328b035572c373f6a40b66e2e3b
Steve Russo <steverusso@pm.me>
2023-03-05 13:46:45 -0500

    lbcli: rename default jot file to scratch

--- lbcli/jot.go
+++ lbcli/jot.go
@@ -17,7 +17,7 @@ type jotCmd struct {
 	//
 	// clap:opt dateit-after,D
 	dateItAfter bool
-	// the target file (defaults to "/jots.md")
+	// the target file (defaults to "/scratch.md")
 	//
 	// clap:opt target,t
 	target string
@@ -30,17 +30,17 @@ type jotCmd struct {
 func (j *jotCmd) run(core lockbook.Core) error {
 	var targetID lockbook.FileID
 	if j.target == "" {
-		// Create a doc named "jots.md" in root if it doesn't exist.
-		f, ok, err := maybeFileByPath(core, "/jots.md")
+		// Create a doc named "scratch.md" in root if it doesn't exist.
+		f, ok, err := maybeFileByPath(core, "/scratch.md")
 		if err != nil {
-			return fmt.Errorf("getting jot file by path: %w", err)
+			return fmt.Errorf("getting scratch file by path: %w", err)
 		}
 		if !ok {
-			f, err = core.CreateFileAtPath("/jots.md")
+			f, err = core.CreateFileAtPath("/scratch.md")
 			if err != nil {
-				return fmt.Errorf("creating '/jots.md': %w", err)
+				return fmt.Errorf("creating '/scratch.md': %w", err)
 			}
-			fmt.Println("created a new '/jots.md' file!")
+			fmt.Println("created a new '/scratch.md' file!")
 		}
 		targetID = f.ID
 	} else {
@@ -52,9 +52,9 @@ func (j *jotCmd) run(core lockbook.Core) error {
 	}
 
 	// Read the target doc's current content.
-	jotsContent, err := core.ReadDocument(targetID)
+	scratchContent, err := core.ReadDocument(targetID)
 	if err != nil {
-		return fmt.Errorf("reading jot file: %w", err)
+		return fmt.Errorf("reading scratch file: %w", err)
 	}
 
 	if j.dateIt || j.dateItAfter {
@@ -72,20 +72,20 @@ func (j *jotCmd) run(core lockbook.Core) error {
 	}
 
 	// Prepend two new lines if the last chars aren't new lines already.
-	n := len(jotsContent)
-	if n > 0 && jotsContent[n-1] != '\n' {
+	n := len(scratchContent)
+	if n > 0 && scratchContent[n-1] != '\n' {
 		j.message = "\n" + j.message
 	}
-	if n > 1 && jotsContent[n-2] != '\n' {
+	if n > 1 && scratchContent[n-2] != '\n' {
 		j.message = "\n" + j.message
 	}
 
 	j.message += "\n"
-	jotsContent = append(jotsContent, j.message...)
+	scratchContent = append(scratchContent, j.message...)
 
 	// Write the new content back.
-	if err = core.WriteDocument(targetID, jotsContent); err != nil {
-		return fmt.Errorf("writing new jots content: %w", err)
+	if err = core.WriteDocument(targetID, scratchContent); err != nil {
+		return fmt.Errorf("writing new scratch content: %w", err)
 	}
 	return nil
 }
